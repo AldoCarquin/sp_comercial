1c3cdb92e1374597cd48b7de15f46310608cc30a
AldoCarquin <aldo.gonzalez.carquin@gmail.com>
2023-02-18 19:46:33 -0300

    18feb1946 ?

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// // General
+// function clickGeneral() {
+//     document.querySelector('.abarrotes').innerHTML = ` GENERAL Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia ullamcorper elementum. Morbi
+// lacinia nunc vel quam feugiat ullamcorper. Sed pulvinar libero ut pellentesque fermentum. GENERAL`;
+// }
+// // Abarrotes
+// function clickAbarrotes() {
+//     document.querySelector('.abarrotes').innerHTML = ` Abarrotes Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia ullamcorper elementum. Morbi
+// lacinia nunc vel quam feugiat ullamcorper. Sed pulvinar libero ut pellentesque fermentum. Abarrotes`;
+// }
+// // Carnes
+// function clickCarnes() {
+//     document.querySelector('.carnes').innerHTML = ` Carnes Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia ullamcorper elementum. Morbi
+// lacinia nunc vel quam feugiat ullamcorper. Sed pulvinar libero ut pellentesque fermentum. Carnes`;
+// }
+// // Licores
+// function clickLicores() {
+//     document.querySelector('.licores').innerHTML = ` Licores Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia ullamcorper elementum. Morbi
+// lacinia nunc vel quam feugiat ullamcorper. Sed pulvinar libero ut pellentesque fermentum. Licores`;
+// }
+// // Vinos
+// function clickVinos() {
+//     document.querySelector('.vinos').innerHTML = ` Vinos Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia ullamcorper elementum. Morbi
+// lacinia nunc vel quam feugiat ullamcorper. Sed pulvinar libero ut pellentesque fermentum. Vinos`;
+// }
+
+// Definimos variable producto
+let producto = 0;
+
+function clickAbarrotes() {
+    if 
+}
